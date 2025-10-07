f46768a92a0e9adb42f9b58b19718c738b98c38a
kuchimpos <kuchimpos@g.ucla.edu>
2025-10-07 09:45:36 -0700

    Add section hash extraction
    Renamed section_info to sections_info for clarity and updated references. Added get_sections_hashes to extract SHA256, SHA1, and MD5 hashes for each section. Deleted duplicate code.

--- peanalyzer.py
+++ peanalyzer.py
@@ -3,10 +3,8 @@ import sys
 import datetime
 from collections import defaultdict
 
-# Things to analyze:
-
 # Condition of sections (e.g writeable when it shouldn't be)
-def section_info(pe: pefile.PE):
+def sections_info(pe: pefile.PE):
   IMAGE_SCN_MEM_EXECUTE = 0x20000000
   IMAGE_SCN_MEM_READ = 0x40000000
   IMAGE_SCN_MEM_WRITE = 0x80000000
@@ -39,119 +37,6 @@ def get_stub(pe: pefile.PE):
   stub_data = pe.get_data(stub_start, stub_end) 
   
   return str(stub_data)
-
-
-  
-
-# Large difference between VirtualSize (size in memory) and SizeOfRawData (disk)
-
-# TimeDateStamp in IMAGE_FILE_HEADER to check when file was created
-def get_timedatestamp(pe: pefile.PE):
-  # Get UNIX timestamp
-  tds = pe.FILE_HEADER.TimeDateStamp
-
-  # Convert to date (YYYY-MM-DD HH:MM:SS)
-  date = datetime.datetime.fromtimestamp(tds)
-  return date
-
-def get_imports(pe: pefile.PE):
-  # List of ImportDescData instances
-  import_table = pe.DIRECTORY_ENTRY_IMPORT
-
-  imp_dlls_and_symbols = defaultdict(list)
-  # For each ImportDescData
-  for imp_desc in import_table:
-    imp_desc_name = imp_desc.dll.decode('utf-8')
-    print("Name: " + imp_desc_name)
-
-    # List of ImportData instances
-    imports = imp_desc.imports
-
-    for imp_data in imports:
-      if imp_data.name is None:
-        # Get ordinal if DLL not imported by name
-        imp_data_name = imp_data.ordinal
-      else: 
-        # Else get name
-        imp_data_name = imp_data.name.decode('utf-8')
-
-      print("Imported symbols: " + imp_data_name)
-      imp_dlls_and_symbols[imp_desc_name].append(imp_data_name)
-      
-  return imp_desc_name
-
-def get_exports(pe: pefile.PE):
-  # ExportDirData instance
-  exp_table = pe.DIRECTORY_ENTRY_EXPORT
-
-  # List of ExportData instances
-  exp_symbols = exp_table.symbols
-
-  # name : (addr, forwarder)
-  symbols = defaultdict(list)
-  for symbol in exp_symbols:
-    if symbol.name is None:
-      # Get ordinal if symbol has no name
-      smbl_name = symbol.ordinal
-    else:
-      # Else get name
-      smbl_name = symbol.name.decode('utf-8')
-
-    smbl_addr = symbol.address
-    smbl_forwarder = symbol.forwarder
-
-    symbols[smbl_name] = (smbl_addr, smbl_forwarder)
-    
-    print(f"Symbol name: {smbl_name}")
-    print(f"Symbol address: {smbl_addr}")
-    print(f"Symbol forwarder: {smbl_forwarder}")
-
-  return symbols
-
-# TLS callbacks - threads set to run code before entry point is ran
-import pefile
-import sys
-import datetime
-from collections import defaultdict
-
-# Things to analyze:
-
-# Condition of sections (e.g writeable when it shouldn't be)
-def section_info(pe: pefile.PE):
-  IMAGE_SCN_MEM_EXECUTE = 0x20000000
-  IMAGE_SCN_MEM_READ = 0x40000000
-  IMAGE_SCN_MEM_WRITE = 0x80000000
-
-  characteristics = []
-  for section in pe.sections:
-    writeable = bool(section.Characteristics & IMAGE_SCN_MEM_WRITE)
-    executable = bool(section.Characteristics & IMAGE_SCN_MEM_EXECUTE)
-    readable = bool(section.Characteristics & IMAGE_SCN_MEM_READ)
-
-    # Decode byte to string and strip null bytes to get just section name
-    section_name = section.Name.decode().strip('\\x00')
-
-    characteristics.append((section_name, section.Misc_VirtualSize, 
-                           section.SizeOfRawData, [writeable, executable, readable]))
-  
-  print("Returning: " + str(characteristics))
-  
-  return characteristics
-  
-# Get stub
-def get_stub(pe: pefile.PE):
-  # RVA of stub start
-  stub_start = 0x40
-
-  # Treat any rich headers as part of stub :P
-  stub_end = pe.DOS_HEADER.e_lfanew
-
-  # Get hex values of stub
-  stub_data = pe.get_data(stub_start, stub_end) 
-  
-  return str(stub_data)
-
-# Large difference between VirtualSize (size in memory) and SizeOfRawData (disk)
 
 # TimeDateStamp in IMAGE_FILE_HEADER to check when file was created
 def get_timedatestamp(pe: pefile.PE):
@@ -307,6 +192,20 @@ def get_sections_entropy(pe: pefile.PE):
     entropies.append((section_name,entropy))
   
   return entropies
+
+def get_sections_hashes(pe: pefile.PE):
+  sect_hashes = {}
+
+  for section in pe.sections:
+    section_name = section.Name.decode().strip('\\x00')
+
+    hashes = {"sha256":section.get_hash_sha256(),
+              "sha1":section.get_hash_sha1(),
+              "md5":section.get_hash_md5()}
+
+    sect_hashes[section_name] = hashes
+  
+  return sect_hashes
   
 # Relocation Table
 def get_reloc_data(pe: pefile.PE):
@@ -327,12 +226,6 @@ def get_reloc_data(pe: pefile.PE):
   
   return data_entries
 
-
-
-
-
-
-
 def main():
   print("In main")
   if len(sys.argv) == 1:
@@ -344,7 +237,7 @@ def main():
   for path in sys.argv:
     pe_files.append(pefile.PE(sys.argv[1]))
 
-  section_info(pe_files[0])
+  sections_info(pe_files[0])
   get_stub(pe_files[0])
   get_timedatestamp(pe_files[0])
   imps = get_imports(pe_files[0])
@@ -374,7 +267,8 @@ def main():
   entropies = get_sections_entropy(pe_files[0])
   print(entropies)
 
-  
+  hashes = get_sections_hashes(pe_files[0])
+  print(hashes)
 
 
   # pe_files[0].print_info()
